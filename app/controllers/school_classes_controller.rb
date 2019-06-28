da80926fc76c271b2af9d5c5baddfde53f0fa896
Alexander Riccio <test35965+learn@gmail.com>
2019-06-28 12:02:03 -0400

    11 failures

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,7 +6,7 @@ class SchoolClassesController < ApplicationController
     end
 
     def index
-
+        @school_classes = SchoolClass.all
     end
 
     def show
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,20 +2,22 @@ class StudentsController < ApplicationController
 
     def create
         @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-
+        redirect_to student_path(@student)
     end
 
     def show
+        # byebug
         @student = Student.find(params[:id])
     end
 
     def index
-
+        @students = Student.all
+        # render 'index'
     end
 
     def new
 
-        redirect_to student_path(Student.last)
+        # redirect_to student_path(Student.last)
     end
 
 
